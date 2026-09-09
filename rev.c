663ece92883d60b7a2703ad5e9cf6313a6af1f9e
vaibhavpatel17 <vaibhavpatel1817@gmail.com>
2026-09-09 14:09:01 +0000

    division

--- rev.c
+++ rev.c
@@ -2,6 +2,8 @@
 int main(){
     int a=10;
     int b=5;
-    printf("addition is=%d",a+b);
+    printf("addition is=%d\n",a+b);
+    printf("subtraction is=%d\n",a-b);
+    printf("division is%d\n",a/b);
     return 0;
 }
