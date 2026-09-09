c018f8a9640b59a616d9061a30e357447d34c591
vaibhavpatel17 <vaibhavpatel1817@gmail.com>
2026-09-09 14:40:24 +0000

    errors about prompts in the scanf and separating the format  specifiers by adding commas

--- rev.c
+++ rev.c
@@ -2,7 +2,7 @@
 int main(){
    int age;float height;
    printf("enter your age and height");
-   scanf("%d %f",&age,&height);
+   scanf("%d,%f",&age,&height);//this is correct only if we enter the inputs separating with commas
    printf("the age and height is %d and %.2f",age,height);
    return 0;
 }
