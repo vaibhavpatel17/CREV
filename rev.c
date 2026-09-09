868634f1226568534404d68becfc8148f29df169
vaibhavpatel17 <vaibhavpatel1817@gmail.com>
2026-09-09 14:19:44 +0000

    improve for only 2 decimal places that is adding .2 after the modulas sign

--- rev.c
+++ rev.c
@@ -3,6 +3,6 @@ int main(){
    int age;float height;
    printf("enter your age and height");
    scanf("%d %f",&age,&height);
-   printf("the age and height is %d and %f",age,height);
+   printf("the age and height is %d and %.2f",age,height);
    return 0;
 }
