3b78bd0b0a5d9b03921acf85e2cecef29c0159a5
vaibhavpatel17 <vaibhavpatel1817@gmail.com>
2026-09-14 07:20:33 +0000

    inverted triangle

--- patterns.c
+++ patterns.c
@@ -2,12 +2,11 @@
 int main(){
     int row;
     int columns;
-    for(row=1;row<=5;row++){
+    for(row=5;row>=0;row--){
         for(columns=1;columns<=row;columns++){
             printf("*");
         }
-        printf("\n");  //after printing *in each row go to the next row 
+        printf("\n");   
     }
-    
     return 0;
 }
