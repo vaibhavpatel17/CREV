625778e9e120c9477b47d28c6670645fc38e1972
vaibhavpatel17 <vaibhavpatel1817@gmail.com>
2026-09-14 07:53:12 +0000

    1 12 123 1234 12345

--- patterns.c
+++ patterns.c
@@ -2,11 +2,14 @@
 int main(){
     int rows;
     int columns;
-    for(rows=3;rows>=0;rows--){
-        for(columns=1;columns<=6;columns++){
-            printf("*");
+    for(rows=1;rows<=5;rows++){
+        for(columns=1;columns<=rows;columns++){
+            printf("%d",columns); // take the value of the column and print that 
+            
+        
         }
         printf("\n");
+    
     }
     return 0;
 }
