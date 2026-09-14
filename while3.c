0824f646b23bb9bd63b472fa5f96e4f130c40258
vaibhavpatel17 <vaibhavpatel1817@gmail.com>
2026-09-14 05:00:44 +0000

    Write a program that repeatedly asks the user for a number:
    Positive → print "Positive"
    Negative → print "Negative"
    0 → stop the program using break
    Keep asking until the user enters 0

--- while3.c
+++ while3.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
-int main(){
-   int i=0;
-   while(i<=50){
-    i++;
-    if(i%2==0){
-           if(i%4==0){
-            continue;
-        }
-        if(i==30){
-            break;
-        }
-          printf("%d",i);
+int main()
+{
+   int i=1;
+   while(i>0||i<0){
+       printf("enter the number:");
+       scanf("%d",&i);
+       if(i==0){
+      break;
    }
-}
+   else if(i>0){
+      printf("POSITIVE\n");
+   }
+    //  continue;  we cannot have selection statements here 
+    //ELSE CANNOT COME INSIDE THE else if statement 
+      else {
+      printf("NEGATIVE\n");
+       
+      }
+   }
+
 return 0;
 }
